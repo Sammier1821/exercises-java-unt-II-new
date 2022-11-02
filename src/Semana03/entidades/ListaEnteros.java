579afb5605f4f2406211d1af07c5c59c222b4893
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-02 12:17:19 -0500

    v1.0

--- src/Semana03/entidades/ListaEnteros.java
+++ src/Semana03/entidades/ListaEnteros.java
@@ -2,47 +2,54 @@ package Semana03.entidades;
 
 public class ListaEnteros {
   private int counter = 0;
-  private int[] array = new int[20];
+  private final int MAX = 20;
+  private int[] integersArray = new int[MAX];
 
   public void addNumber(int number) {
-    array[counter] = number;
+    integersArray[counter] = number;
     counter++;
   }
   
   public void removeNumber(int index) {
+    int n;
     if (index < counter) {
-      array[index] = 0;
+      integersArray[index] = 0;
+      for (int i = index; i < counter - 1; i++) {
+        n = integersArray[i];
+        integersArray[i] = integersArray[i+1];
+        integersArray[i+1] = n;
+      }
       counter--;
     } else
       System.out.println("Index out of range (counter)");
   }
   
   public int getNumber(int index) {
-    return array[index];
+    return integersArray[index];
   }
   
   public boolean existInArray(int number) {
     for (int i = 0; i < counter; i++)
-      if (array[i] == number)
+      if (integersArray[i] == number)
         return true;
     return false;
   }
   
   public void setNumberOnIndex(int number, int index) {
     if (index < counter)
-      array[index] = number;
+      integersArray[index] = number;
     else
-      System.out.println("Index out of range (counter)");
+      System.out.println("Indice fuera de rango (" + 0 + "-" + (counter-1) + ")");
   }
   
   public void numbersInDescendingOrder() {
     int x;
     for (int i = 0; i < counter; i++)
       for (int j = i + 1; j < counter; j++)
-        if (array[i] <= array[j]) {
-          x = array[i];
-          array[i] = array[j];
-          array[j] = x;
+        if (integersArray[i] <= integersArray[j]) {
+          x = integersArray[i];
+          integersArray[i] = integersArray[j];
+          integersArray[j] = x;
         }
   }
   
@@ -51,6 +58,6 @@ public class ListaEnteros {
       System.out.println("Numbers does not exist in the array");
     else
       for (int i = 0; i < counter; i++)
-        System.out.println("array[" + i + "] = " + array[i]);
+        System.out.println("array[" + i + "] = " + integersArray[i]);
   }
 }
