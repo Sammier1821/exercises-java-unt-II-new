072c31f16bc8e9ebadbebeb899bdb413f7d86167
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-01 22:13:15 -0500

    v1.0

---
+++ src/Semana02/presentacion/Ejercicio19.java
@@ -0,0 +1,46 @@
+package Semana02.presentacion;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ * App to convert a decimal number into a binary
+ * @author Sammier1821
+ */
+public class Ejercicio19 {
+  public static Scanner sc = new Scanner(System.in);
+
+  public static boolean control(String x) {
+    char resp;
+    do {
+      System.out.println();
+      System.out.print(x);
+      System.out.print(" (y/n): ");
+      resp = sc.nextLine().toLowerCase().charAt(0);
+    } while (resp != 'y' && resp != 'n');
+    if (resp == 'y')
+      return true;
+    return false;
+  }
+  
+  public static void title(String phrase) {
+    System.out.println("\n\t" + phrase + "\n");
+  }
+  
+  public static void main(String[] args) {
+    int num = 0, counter = 0, remainder = 0, binary = 0;
+    do {
+      title("App to convert a decimal number into a binary");
+      System.out.print("Enter a number: ");
+      num = sc.nextInt();
+      sc.nextLine();
+      while (num >= 1) {
+        remainder = num % 2;
+        num /= 2;
+        binary += (int) (remainder * Math.pow(10, counter));
+        counter++;
+      }
+      System.out.println("The binary number is: " + binary);
+    } while (control("Do you want to restart the app?"));
+  }
+}
