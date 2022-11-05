aaaa6b3911074d358bb8da9212656d829294966f
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-05 17:08:49 -0500

    v1.0

---
+++ src/Semana03/entidades/MovimientoProducto.java
@@ -0,0 +1,45 @@
+package Semana03.entidades;
+
+public class MovimientoProducto {
+  private String productCode;
+  private int quantity;
+  private int movementType;
+
+  public void setProductCode(String productCode) {
+    this.productCode = productCode;
+  }
+
+  public void setQuantity(int quantity) {
+    if (quantity > 0)
+      this.quantity = quantity;
+    else
+      System.out.println("La cantidad está fuera de rango (<=0)");
+  }
+
+  public void setMovementType(int movementType) {
+    if (movementType == 0 || movementType == 1)
+      this.movementType = movementType;
+    else
+      System.out.println("El tipo de movimiento es solo 1 (entrada) o 0 (salida)");
+  }
+
+  public String getProductCode() {
+    return productCode;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public int getMovementType() {
+    return movementType;
+  }
+
+  @Override
+  public String toString() {
+    if (movementType == 0)
+      return "Salida: " + getProductCode() + " ===== " + getQuantity();
+    else
+      return "Entrada: " + getProductCode() + " ===== " + getQuantity();
+  }
+}
