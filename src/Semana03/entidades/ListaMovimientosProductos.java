de9e5e54711af7569b461f73ff2b67e8e8721f8d
Sammier1821 <productive.gian.112020@gmail.com>
2022-11-07 07:53:05 -0500

    v0.2 Need to rewrite

--- src/Semana03/entidades/ListaMovimientosProductos.java
+++ src/Semana03/entidades/ListaMovimientosProductos.java
@@ -7,11 +7,38 @@ public class ListaMovimientosProductos {
   private static final int MAX = 10;
   private static MovimientoProducto[] productMvmArray = new MovimientoProducto[MAX];
   
-  private static final java.util.Scanner sc = new java.util.Scanner(System.in);
+  public static void addProductMvm(MovimientoProducto x) {
+    productMvmArray[counter] = x;
+    counter++;
+  }
   
-  public static void addProductMvm() {
-    System.out.println("Enter product code: ");
-    productMvmArray[counter].setProductCode(sc.nextLine());
-    
+  public static int getStock(String productCode) {
+    int quantity = 0;
+    for (int i = 0; i < counter; i++) {
+      if (productMvmArray[i].getProductCode().equals(productCode)) {
+        quantity += productMvmArray[i].getQuantity();
+      }
+    }
+    return quantity;
+  }
+  
+  public static void showProductMvms() {
+    for (int i = 0; i < counter; i++)
+      System.out.println((i < 10 ? "0" + i : i) + ": " + productMvmArray[i]);
+  }
+  
+  public static void showOrderedProductMvms() {
+    MovimientoProducto[] orderedArray = new MovimientoProducto[MAX];
+    MovimientoProducto x;
+    orderedArray = productMvmArray;
+    for (int i = 0; i < counter; i++)
+      for (int j = i + 1; j < counter; j++)
+        if (orderedArray[i].getQuantity() <= orderedArray[j].getQuantity()) {
+          x = orderedArray[i];
+          orderedArray[i] = orderedArray[j];
+          orderedArray[j] = x;
+        }
+    for (int i = 0; i < counter; i++)
+      System.out.println((i < 10 ? "0" + i : i) + ": " + orderedArray[i]);
   }
 }
